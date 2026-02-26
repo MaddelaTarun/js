ff7a4247727351e499995f1db84a518cac206b55
Tarun <maddelatarun0@gmail.com>
2026-02-26 21:33:21 +0530

    completed objects

--- 2-intermediate/objects.js
+++ 2-intermediate/objects.js
@@ -32,61 +32,58 @@ user.greeting();              // Executes the function (Prints: Hello, Tarun)
 
 // Singleton
 
-const tinderUser1 = new Object(); // Singleton Object
-const tinderUser2 = {} // Non-Singleton Object
+const tinderUser1 = new Object(); // Singleton Object (Constructor)
+const tinderUser2 = {};           // Non-Singleton Object (Literal)
 
 tinderUser1.id = "123abc";
 tinderUser1.name = "Sammy";
 tinderUser1.isLoggedIn = false;
 
-console.log(tinderUser1)
-console.log(tinderUser2)
-
 const anotherUser = {
     email: "some@gmail.com",
     fullname: {
-	userFullName: {
-	    firstName: "M",
-	    lastName: "Tarun"
-	}
+        userFullName: {
+            firstName: "M",
+            lastName: "Tarun"
+        }
     }
 }
 
-console.log(anotherUser["fullname"]);
-
+console.log(anotherUser["fullname"]); 
 
 const obj1 = {1: "a", 2: "b"};
 const obj2 = {3: "c", 4: "d"};
-const obj3 = Object.assign(obj1,obj2);
-console.log(obj3);
-const obj4 = Object.assign({},obj1,obj2);
-console.log(obj4);
-const obj5 = {...obj1,...obj2};
-console.log(obj5);
+
+const obj3 = Object.assign(obj1, obj2); 
+
+const obj4 = Object.assign({}, obj1, obj2); 
+
+const obj5 = {...obj1, ...obj2}; 
 
 const users = [
-    {
-	id: 1,
-	email: "a@gmail.com"
-    },
-    {
-	id: 2,
-	email: "b@gmail.com"
-    },
-    {
-	id: 3,
-	email: "c@gmail.com"
-    }
+    { id: 1, email: "a@gmail.com" },
+    { id: 2, email: "b@gmail.com" },
+    { id: 3, email: "c@gmail.com" }
 ]
 
-console.log(users[0].id);
-console.log(users[0].email);
-console.log(users[1].id);
-console.log(users[1].email);
+console.log(users[1].email); // Returns "b@gmail.com"
 
-console.log(Object.keys(tinderUser1));
-console.log(Object.values(tinderUser1));
-console.log(Object.entries(tinderUser1));
+console.log(Object.keys(tinderUser1));   // [ 'id', 'name', 'isLoggedIn' ]
+console.log(Object.values(tinderUser1)); // [ '123abc', 'Sammy', false ]
+console.log(Object.entries(tinderUser1));// [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], ... ]
 
-console.log(tinderUser1.hasOwnProperty('isLoggedIn'));
-console.log(tinderUser1.hasOwnProperty('isLogged'));
+console.log(tinderUser1.hasOwnProperty('isLoggedIn')); // true
+
+const course = {
+    courseName: "JS",
+    price: "999",
+    teacher: "T",
+}
+
+// Basic Destructuring: Extracting 'teacher' as a standalone variable
+const {teacher} = course;
+console.log(teacher); // "T"
+
+// Renaming during Destructuring: Extracting 'teacher' but calling it 'by'
+const {teacher: by} = course;
+console.log(by); // "T"

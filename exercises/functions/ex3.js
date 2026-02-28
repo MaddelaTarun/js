1b26c85bda7eded02a3f884eea02b8f8f20946dd
Tarun <maddelatarun0@gmail.com>
2026-02-28 21:10:10 +0530

    functions exercises completed

--- exercises/functions/ex3.js
+++ exercises/functions/ex3.js
@@ -3,3 +3,11 @@
     If a city is passed, it returns "The weather in [city] is sunny".
     Use a Default Parameter so that if no city is passed, it defaults to "Mumbai".
 */
+
+
+function getWeather(city = "Mumbai") {
+    return `The weather in ${city} is sunny`
+}
+
+console.log(getWeather("Bengaluru"));
+console.log(getWeather());

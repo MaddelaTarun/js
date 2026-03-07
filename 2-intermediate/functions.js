9a40bdaec992f816a8efef8f574e2a575b6d3d2f
Tarun <maddelatarun0@gmail.com>
2026-03-07 21:31:06 +0530

    functions completed

--- 2-intermediate/functions.js
+++ 2-intermediate/functions.js
@@ -19,3 +19,42 @@ function loginUserMessage(username) {
 
 console.log(loginUserMessage("T")); // "T just logged in!"
 console.log(loginUserMessage());    // Logs "Please enter..." and returns undefined
+
+// Functions -- Continued
+
+function calculatePrice(val1,val2,...num1) {
+    return num1;
+}
+
+console.log(calculatePrice(200,400,500,700));
+
+
+function calculatedPrice(...num1) {
+    return num1;
+}
+
+console.log(calculatedPrice(200,400,500,700));
+
+const user = {
+    username: "Tarun",
+    age: 20
+}
+
+function handleObject(anyObject) {
+    console.log(`Username is ${anyObject.username}, age is ${anyObject.age}`);
+}
+
+handleObject(user);
+
+handleObject({
+    username: "T",
+    age: 20
+})
+
+const newArray = [10,20,40];
+
+function returnSecVal(getArray) {
+    return getArray[0];
+}
+
+console.log(returnSecVal(newArray));
